6a6cf58fe63e8866cf50963604094b1bb6ce9212
Takumi Suzuki <devtkmszk824@gmail.com>
2024-06-04 20:28:54 +0900

    Initial commit with project setup and configuration

---
+++ src/main/kotlin/com/devtkms/mymenuappspring/MyMenuAppSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.devtkms.mymenuappspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyMenuAppSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<MyMenuAppSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/devtkms/mymenuappspring/MyMenuAppSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devtkms.mymenuappspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyMenuAppSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
